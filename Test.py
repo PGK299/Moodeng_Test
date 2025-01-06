1e9e95b07ecd25251e2a7d8e1168a7c7a54693ab
Atiwich <atiwich1108@gmail.com>
2025-01-06 18:02:46 +0700

    first commit

---
+++ Test.py
@@ -0,0 +1,12 @@
+a = 20
+b = 30
+c = a+b
+name = "Moo"
+surname = "Deng"
+print(f"Hello! {name} {surname} nice to meet you.")
+print(c)
+print("---------------------")
+for i in range(1,11):
+    print(i)
+    i+=1
+print("---------------------")
